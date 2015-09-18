68f0b81823212aed04a261c602a2090728e5b1c9
GentlemanBrewing <nelisvolschenk@gmail.com>
2015-09-18 16:36:09 +0200

    Update Readvoltage.py

--- Readvoltage.py
+++ Readvoltage.py
@@ -1,5 +1,22 @@
+#!/usr/bin/python3
 # Convert Voltage from ADC to Temperature Reading
 
-def Voltage_to_Temperature(voltage)
-  Temp = 
+from ABE_ADCPi import ADCPi
+from ABE_helpers import ABEHelpers
+import time
+import os
+
+i2c_helper = ABEHelpers()
+bus = i2c_helper.get_smbus()
+adc = ADCPi(bus, 0x68, 0x69, 12)
+
+
+def Voltage(channel)
+  voltage = adc.readvoltage(channel)
+  return voltage
+  
+def Resistance(voltage)
+  resistance = (R2 * R3 + R3 * (R1 + R2) * (Vb / Vin)) / (R1 - (R1 + R2) * (Vb / Vin))
+  return resistance
+  
   
