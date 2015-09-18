e091ca18f37df15702e099929ab122fdbfe085e8
GentlemanBrewing <nelisvolschenk@gmail.com>
2015-09-18 17:01:38 +0200

    Update Readvoltage.py

--- Readvoltage.py
+++ Readvoltage.py
@@ -8,15 +8,21 @@ import os
 
 i2c_helper = ABEHelpers()
 bus = i2c_helper.get_smbus()
-adc = ADCPi(bus, 0x68, 0x69, 12)
+adc = ADCPi(bus, 0x68, 0x69, 16)
 
-
-def Voltage(channel)
-  voltage = adc.readvoltage(channel)
-  return voltage
   
-def Resistance(voltage)
-  resistance = (R2 * R3 + R3 * (R1 + R2) * (Vb / Vin)) / (R1 - (R1 + R2) * (Vb / Vin))
-  return resistance
+def Resistance(self, channel)
+  # Default Resistance values in wheatstone bridge
+  R1 = 3300
+  R2 = 100
+  R3 = 3300
+  Vin = 3.3
+  Vb = adc.read_voltage(channel)
+  # Wheatstone bridge calculation
+  Rx = (R2 * R3 + R3 * (R1 + R2) * (Vb / Vin)) / (R1 - (R1 + R2) * (Vb / Vin))
+  return Rx
   
-  
+def Temperature(self, channel)
+  resistance = self.Resistance(channel)
+  t = (resistance - 100) / 38.51 ) * 100
+  return t
