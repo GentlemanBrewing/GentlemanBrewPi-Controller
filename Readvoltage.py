1c8a816704722c0cad854d5ba07e41f8cf13316a
GentlemanBrewing <nelisvolschenk@gmail.com>
2015-09-30 11:49:14 +0200

    Update Readvoltage.py

--- Readvoltage.py
+++ Readvoltage.py
@@ -10,31 +10,48 @@ i2c_helper = ABEHelpers()
 bus = i2c_helper.get_smbus()
 adc = ADCPi(bus, 0x68, 0x69, 16)
 
+class ADCTEMP:
   
-def Resistance(channel):
-  # Default Resistance values in wheatstone bridge
-  R1 = 3300
-  R2 = 100
-  R3 = 3300
-  Vin = 3.3
-  Vb = adc.read_voltage(channel)
-  # Wheatstone bridge calculation
-  Rx = (R2 * R3 + R3 * (R1 + R2) * (Vb / Vin)) / (R1 - (R1 + R2) * (Vb / Vin))
-  return Rx
+  #Initialize Variables
+  __R1 = 0
+  __R2 = 0
+  __R3 = 0
+  __Rx = 0.0
+  __Vin = 0.0
+  
+  def __init__(self, channel, R1=3300, R2=100, R3=3300, Vin=3.3)
+    self.__R1 = R1
+    self.__R2 = R2
+    self.__R3 = R3
+    self.__Vin = Vin
+  
+  def Resistance(self, channel):
+   Vb = adc.read_voltage(channel)
+    #Input variables
+    R1 = self.__R1
+    R2 = self.__R2
+    R3 = self.__R3
+    Vin = self.__Vin
+    # Wheatstone bridge calculation
+    Rx = (R2 * R3 + R3 * (R1 + R2) * (Vb / Vin)) / (R1 - (R1 + R2) * (Vb / Vin))
+    return Rx
 
-# Calculate Temperature for PT100  
-def Temperature(channel):
-  resistance = Resistance(channel)
-  t = ((resistance - 100) / 38.51 ) * 100
-  return t
+  # Calculate Temperature for PT100  
+  def Temperature(self, channel):
+    resistance = self.Resistance(channel)
+    t = ((resistance - 100) / 38.51 ) * 100
+    return float(t)
   
-while (True):
-  os.system('clear')
-  adc.set_pga(8)
+  while (True):
+    os.system('clear')
+    adc.set_pga(8)
 
-  print (adc.read_raw(1))
-  print (adc.read_voltage(1))
-  print (Resistance(1))
-  print (Temperature(1))
+    print (adc.read_raw(1))
+    print (adc.read_voltage(1))
+    print (Resistance(1))
+    print (Temperature(1))
   
-  time.sleep (5)
+    time.sleep (5)
+
+  
+  
