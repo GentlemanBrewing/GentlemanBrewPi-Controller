e859ff415d2fe64d5ac6318a90a4c59aa42117a1
GentlemanBrewing <nelisvolschenk@gmail.com>
2016-07-30 23:05:06 +0200

    Error Checking

--- Main.py
+++ Main.py
@@ -32,10 +32,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.OUT)
 while (True):
 	temperature = adc.Temperature(1)
-	if temperature > 10 :
+	if temperature > Setpoint +1:
 		GPIO.output(27, True)
-	#elif temperature < Setpoint -1:
-		#GPIO.output(27, False)
+	elif temperature < Setpoint -1:
+		GPIO.output(27, False)
 	
 	print(temperature)
+	print(Setpoint)
 	time.sleep(5)
