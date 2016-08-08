8fc2b42030ebfa13627632dc9620312d1e4b6417
GentlemanBrewing <nelisvolschenk@gmail.com>
2016-08-08 16:07:07 +0200

    Update BrewManager.py

--- BrewManager.py
+++ BrewManager.py
@@ -12,19 +12,35 @@ class brewmanager(multiprocessing.process):
   
   def __init__(self, inputqueue, outputqueue):
     multiprocessing.Process.__init__(self)
+    self.processinformation = loadconfig()
     
   # Function for loading config file
-  def Loadconfig():
+  def loadconfig():
 	  f = open('Config.yaml')
-	  dataMap = yaml.safe_load(f)
+	  datamap = yaml.safe_load(f)
 	  f.close
-  return dataMap
+  return datamap
   
   # Function for updating config file
-  def Writeconfig(data):
+  def writeconfig(data):
   	f = open('Config.yaml', "w")
   	yaml.dump(data, f)
   f.close
   
+  # Check processes to spawn
+  def parse_config:
+  	
+  		
   
-    
+  # Write to database
+  def database_write:
+  	
+  
+  
+  
+  if __name__ == 'main':
+  	# Create processes for each process in config file
+  	for each process in self.processinformation.items():
+		process['process_data']['object'] = Worker()
+  		
+  	
