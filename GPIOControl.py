3ba78433712cbc569e538c9733f0824a0cca9178
GentlemanBrewing <nelisvolschenk@gmail.com>
2016-08-10 18:30:04 +0200

    Writing Main code

--- GPIOControl.py
+++ GPIOControl.py
@@ -4,24 +4,24 @@ import RPi.GPIO as GPIO
 import time
 
 
-gpiopins = {'4': GPIO.OUT,
-            '17': GPIO.OUT,
-            '27': GPIO.OUT,
-            '22': GPIO.OUT,
-            '10': GPIO.OUT,
-            '9': GPIO.OUT,
-            '11': GPIO.OUT,
-            '5': GPIO.OUT,
-            '6': GPIO.IN,
-            '14': GPIO.OUT,
-            '15': GPIO.OUT,
-            '18': GPIO.OUT,
-            '23': GPIO.OUT,
-            '24': GPIO.OUT,
-            '25': GPIO.OUT,
-            '8': GPIO.OUT,
-            '7': GPIO.OUT,
-            '12': GPIO.OUT, }
+gpiopins = {'4': 'GPIO.OUT',
+            '17': 'GPIO.OUT',
+            '27': 'GPIO.OUT',
+            '22': 'GPIO.OUT',
+            '10': 'GPIO.OUT',
+            '9': 'GPIO.OUT',
+            '11': 'GPIO.OUT',
+            '5': 'GPIO.OUT',
+            '6': 'GPIO.IN',
+            '14': 'GPIO.OUT',
+            '15': 'GPIO.OUT',
+            '18': 'GPIO.OUT',
+            '23': 'GPIO.OU',
+            '24': 'GPIO.OUT',
+            '25': 'GPIO.OUT',
+            '8': 'GPIO.OUT',
+            '7': 'GPIO.OUT',
+            '12': 'GPIO.OUT'}
 
 GPIO.setmode(GPIO.BCM)
 
