9ca923dc85e2e204c1d0adfb19d7b61b50e3f7bd
GentlemanBrewing <nelisvolschenk@gmail.com>
2016-08-12 20:46:47 +0200

    Writing Main code

--- Controller.py
+++ Controller.py
@@ -222,7 +222,7 @@ class PIDController(multiprocessing.Process):
 
             # Calculate PWM duty needed from ssr
             ssroutput = output - relayoutput
-            ssr_pwmduty = ssroutput // ssrduty * 100
+            ssr_pwmduty = ( ssroutput * 100 ) // ssrduty
             print(ssr_pwmduty)
 
             # Activate pins to switch relays
