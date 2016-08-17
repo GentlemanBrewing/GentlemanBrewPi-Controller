12017544ef3f454951557d190eb7653b19f1b158
GentlemanBrewing <nelisvolschenk@gmail.com>
2016-08-17 15:43:17 +0200

    Writing Main code

--- WebTester.py
+++ WebTester.py
@@ -3,20 +3,32 @@
 import multiprocessing
 import queue
 import time
+import yaml
 import WebServer
 
 class TestWeb(multiprocessing.Process):
     def __init__(self):
         multiprocessing.Process.__init__(self)
+        self.processinformation = self.loadconfig('Config.yaml')
         self.processes = {'Steam Boiler': 90, 'Fermenter1': 17, 'Fermenter2': 18, 'Fermenter3': 19,}
-        self.outputdict = {}
+        self.process_output = {}
+        self.process_output = self.processes
 
         # create communication queues
-        self.inputqueue = multiprocessing.Queue
-        self.outputqueue = multiprocessing.Queue
+        self.inputqueue = multiprocessing.Queue()
+        self.outputqueue = multiprocessing.Queue()
+
+    # Function for loading config file
+    def loadconfig(self, filename):
+        f = open(filename)
+        datamap = yaml.safe_load(f)
+        f.close()
+        return datamap
 
     def run(self):
-        #start web server
+        # put data in queue
+        self.inputqueue.put(self.process_output)
+        # start web server
         WebServer.main(self.inputqueue, self.outputqueue)
         while True:
             # Generate data for webserver
@@ -29,9 +41,13 @@ class TestWeb(multiprocessing.Process):
                     if temp > 22:
                         temp = 17
                 self.processes[process] = temp
+                self.process_output[process] = temp
 
             # Put updated data in queue
-            self.outputqueue.put_nowait(self.processes)
+            self.outputqueue.put(self.process_output)
+
+            #Sleep
+            time.sleep(1)
 
 
 
