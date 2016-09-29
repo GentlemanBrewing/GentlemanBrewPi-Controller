3e3be4c2f54a6718c33164bee32227b4e79c95e8
GentlemanBrewing <nelisvolschenk@gmail.com>
2016-09-29 11:51:42 +0200

    Writing Main code

--- Controller.py
+++ Controller.py
@@ -284,7 +284,7 @@ class PIDController(multiprocessing.Process):
                     updated_variables = self.inputqueue.get_nowait()
                     for variable, value in updated_variables.items():
                         self.variabledict[variable] = value
-                    print(self.variabledict)
+                        print('%s : %s' % variable, value)
             except queue.Empty:
                 pass
 
